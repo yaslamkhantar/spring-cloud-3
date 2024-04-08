8d95fd0d5bc38fabf5b3ae1fd2ce82e300fea283
unknown <yassine.moukhantar002@gmail.com>
2024-04-08 16:17:44 +0200

    resil4j->True

--- account-service/src/main/java/me/elmoukhantar/accountservice/clients/CustomerRestClient.java
+++ account-service/src/main/java/me/elmoukhantar/accountservice/clients/CustomerRestClient.java
@@ -1,5 +1,6 @@
 package me.elmoukhantar.accountservice.clients;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import me.elmoukhantar.accountservice.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +11,17 @@ import java.util.List;
 @FeignClient(name = "CUSTOMER-SERVIE")
 public interface CustomerRestClient {
     @GetMapping("ap1/custom/{id}")
+   // @CircuitBreaker(name = "customeservi" ,fallbackMethod = "getmessage")
     Customer findbyone(@PathVariable Long id);
+
+   /* default Customer getmessage(Long id){
+        Customer customer=new Customer();
+        customer.setId(id);
+        customer.setFirstname("server busy");
+        customer.setLastname("--- busy");
+        customer.setEmail("not available");
+        return customer;
+    }*/
+
+
 }
